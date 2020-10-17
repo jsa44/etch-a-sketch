1bca668bf29df72c795ed7375a2c00caea585500
Juan Saavedra <saavedra.juan.p@gmail.com>
2020-10-17 16:25:09 -0400

    add reset button

--- myScript.js
+++ myScript.js
@@ -1,7 +1,23 @@
 
-for (i=0; i<16; i++) {
-    const container = document.querySelector('#container');
-    const box = document.createElement('div');
-    box.classList.toggle('box');
-    container.appendChild(box);
+const container = document.querySelector('#container');
+
+createGrid()
+
+function createGrid() {
+    while (container.firstChild) {
+        container.removeChild(container.firstChild)
+    }
+    for (i=0; i<16; i++) {
+        const box = document.createElement('div');
+        box.classList.toggle('box');
+        box.addEventListener('mouseenter', () => {
+            box.classList.add('filled-box');
+        });
+        container.appendChild(box);
+    }
 }
+
+const button = document.querySelector('#reset');
+button.addEventListener('click', () => {
+    createGrid();
+});
